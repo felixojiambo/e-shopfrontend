7b20b58c86ee6864bf05978107ddb33763f4aa0b
Felix Ojiambo <felixojiamboe@gmail.com>
2024-04-06 09:16:32 -0700

    setup routes in app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
 import './App.css';
 import Navbar from './components/navbar/Navbar';
-
+import { BrowserRouter,Routes,Route } from 'react-router-dom';
+import Shop from './pages/Shop';
+import ShopCategory from './pages/ShopCategory';
+import Product from './pages/Product';
+import Cart from './pages/Cart';
+import LoginSignUp from './pages/LoginSignUp';
 function App() {
   return (
     <div>
+      <BrowserRouter>
       <Navbar />
+      <Routes>
+
+        <Route path='/' element={<Shop/>} />
+        <Route path='/mens' element={<ShopCategory category="men"/>} />
+        <Route path='/womens' element={<ShopCategory category="women"/>} />
+        <Route path='/kids' element={<ShopCategory category="kid"/>} />
+       <Route path='/product' element={<Product/>}>
+        <Route path=':productid' element={<Product/>}/>
+       </Route>
+       <Route>
+       <Route path='/cart' element={<Cart/>} />
+       <Route path='/login' element={<LoginSignUp/>} />
+       </Route>
+      </Routes>
+    
+      </BrowserRouter>
     </div>
   );
 }
