8df4720a7745563e8100ba7b40b266ba05b4ed4e
Felix Ojiambo <felixojiamboe@gmail.com>
2024-04-06 08:53:14 -0700

    h line undeline

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -9,23 +9,21 @@ const [menu,setMenu]=useState("shop");
 
 
   return (
-    <div className='navbar'>
-<div className='nav-logo'>
-<img src={logo} alt=""/>
-<p>E-SHOP</p>
-</div>
-<uL className="nav-menu">
-<li onClick={()=>{setMenu("shop")}}>Shop<hr/></li>
-<li onClick={()=>{setMenu("mens")}}>Men</li>
-<li onClick={()=>{setMenu("womens")}}>Women</li>
-<li onClick={()=>{setMenu("kids")}}>Kids</li>
-</uL>
-
-<div className='nav-login-cart'>
-<button>Login</button>
-<img src={cart_icon} alt=''/>
-<div className="nav-cart-count">0</div>
-</div>
+    <><div className='navbar'>
+      <div className='nav-logo'>
+        <img src={logo} alt="" />
+        <p>E-SHOP</p>
+      </div>
+      <uL className="nav-menu">
+        <li onClick={() => { setMenu("shop") } }>Shop{menu === "shop" ? <h /> : <></>}</li>
+      <li onClick={() => { setMenu("mens") } }>Men{menu === "mens" ? <h /> : <></>}</li>
+      <li onClick={() => { setMenu("womens") } }>Women{menu === "womens" ? <h /> : <></>}</li>
+      <li onClick={() => { setMenu("kids") } }>Kids{menu === "kids" ? <h /> : <></>}</li>
+    </uL><div className='nav-login-cart'>
+        <button>Login</button>
+        <img src={cart_icon} alt='' />
+        <div className="nav-cart-count">0</div>
+      </div></>
     </div>
   )
 }
