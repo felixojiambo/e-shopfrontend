df1285077e54b0e3f77c78b9dbbfe078e7511477
Felix Ojiambo <felixojiamboe@gmail.com>
2024-04-20 09:09:45 -0700

    shopcategory return

--- src/App.js
+++ src/App.js
@@ -7,6 +7,9 @@ import Product from './pages/Product';
 import Cart from './pages/Cart';
 import Footer from './components/Footer/Footer';
 import LoginSignUp from './pages/LoginSignUp';
+import men_banner from './components/assets/banner_mens.png';
+import women_banner from'./components/assets/banner_women.png';
+import kid_banner from './components/assets/banner_kids.png';
 function App() {
   return (
     <div>
@@ -15,9 +18,9 @@ function App() {
       <Routes>
 
         <Route path='/' element={<Shop/>} />
-        <Route path='/mens' element={<ShopCategory category="men"/>} />
-        <Route path='/womens' element={<ShopCategory category="women"/>} />
-        <Route path='/kids' element={<ShopCategory category="kid"/>} />
+        <Route path='/mens' element={<ShopCategory banner={men_banner} category="men"/>} />
+        <Route path='/womens' element={<ShopCategory banner={women_banner} category="women"/>} />
+        <Route path='/kids' element={<ShopCategory banner={kid_banner} category="kid"/>} />
        <Route path='/product' element={<Product/>}>
         <Route path=':productid' element={<Product/>}/>
        </Route>
