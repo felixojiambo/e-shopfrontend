72ca756d3dae1b5b58dd3d7cf7956c24f8b882e7
Felix Ojiambo <felixojiamboe@gmail.com>
2024-04-21 04:19:09 -0700

    login page

--- src/pages/LoginSignUp.jsx
+++ src/pages/LoginSignUp.jsx
@@ -2,7 +2,24 @@ import React from 'react'
 
 const LoginSignUp = () => {
   return (
-    <div>LoginSignUp</div>
+    <div className="loginsignup">
+
+      <div className="loginsignup-container"> <h1>Sign Up </h1>
+      
+      <div className="loginsignup-fields">
+        <input type="tetx" placeholder="Your name" />
+        <input type="email" placeholder="Email Address" />
+        <input type="password" placeholder="Password" />
+      </div>
+      <button>Continue</button>
+
+      <p className="loginsignup-login">Already have an account? <span>Login here</span></p>
+      <div className="loginsignup-agree">
+        <input type="checkbox" name="" id="" />
+        <p>By continue, I agree to follow the terms and privacy policy</p>
+      </div>
+       </div>
+    </div>
   )
 }
 
