593014fdf2808355856eb63d744013e810c1b149
Felix Ojiambo <felixojiamboe@gmail.com>
2024-04-21 11:17:50 -0700

    get total cart items

--- src/context/ShopContext.jsx
+++ src/context/ShopContext.jsx
@@ -35,7 +35,17 @@ const ShopContextProvider = (props) => {
         return totalAmount;
     }
  }
- const contextValue = {getTotalCartAmount, all_product ,cartItems,addToCart,removeToCart};
+ const getTotalCartItems=()=>{
+let totalItem=0;
+for(const item in cartItems)
+{
+    if (cartItems[item]>0){
+        totalItem+=cartItems[item];
+    }
+}
+return totalItem;
+ }
+ const contextValue = {getTotalCartAmount,getTotalCartItems, all_product ,cartItems,addToCart,removeToCart};
   return (
     <ShopContext.Provider value={contextValue}>
       {props.children}
