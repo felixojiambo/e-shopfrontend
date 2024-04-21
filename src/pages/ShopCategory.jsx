b247e8a1d088a48bea92a3aead5c985c9f617787
Felix Ojiambo <felixojiamboe@gmail.com>
2024-04-21 04:11:30 -0700

    shop category css

--- src/pages/ShopCategory.jsx
+++ src/pages/ShopCategory.jsx
@@ -7,7 +7,7 @@ const ShopCategory = (props) => {
   const {all_product}=useContext(ShopContext);
   return (
     <div className="shop-category">
-<img src={props.banner} alt='' />
+<img className="shopcategory-banner" src={props.banner} alt='' />
 <div className="shopcategory-indexSort">
   <p>
     <span>Showing 1-12</span> out of 36 producst
@@ -17,7 +17,7 @@ const ShopCategory = (props) => {
   </div>
 </div>
 <div className="shopcategory-products">
-  {all_product.map((item)=>{
+  {all_product.map((item,i)=>{
     if(props.category===item.category){
 return <Item  key={i}
 id={item.id}
@@ -27,6 +27,9 @@ new_price={item.new_price}
 old_price={item.old_price}/>
 
   }else {return null;}})}
+</div>
+<div className="shopcategory-loadmore">
+  Explore More
 </div>
     </div>
   )
