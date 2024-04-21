7b73ac1e0b07c2f3e17b608bfb05912aa09f3b97
Felix Ojiambo <felixojiamboe@gmail.com>
2024-04-21 09:54:23 -0700

    add to cart funtion in context api

--- src/context/ShopContext.jsx
+++ src/context/ShopContext.jsx
@@ -1,10 +1,34 @@
-import React, { createContext } from "react";
-import all_product from "../components/assets/all_product"
-export const ShopContext=createContext(null);
-const ShopContextProvider=(props)=>{
-    const contextValue={all_product};
-    return (
-        <ShopContext.Provider value={contextValue}>{props.children}</ShopContext.Provider>
-    )
-}
-export default ShopContextProvider
+import React, { createContext, useState } from "react";
+import all_product from "../components/assets/all_product";
+export const ShopContext = createContext(null);
+const getDefaultCart=()=>{
+    let cart={};
+    for(let index=0;index<all_product.length+1;index++){
+       cart[index]=0;
+
+    }
+    return cart;
+  }
+const ShopContextProvider = (props) => {
+ 
+ const [cartItems,setCartItems]=useState(getDefaultCart());
+ //const contextValue = { all_product ,cartItems};
+
+ //console.log(cartItems);
+
+ const addToCart=(itemId)=>{
+    setCartItems((prev)=>({...prev,[itemId]:prev[itemId]+1}))
+ }
+
+ const removeToCart=(itemId)=>{
+    setCartItems((prev)=>({...prev,[itemId]:prev[itemId]-1}))
+
+ }
+ const contextValue = { all_product ,cartItems,addToCart,removeToCart};
+  return (
+    <ShopContext.Provider value={contextValue}>
+      {props.children}
+    </ShopContext.Provider>
+  );
+};
+export default ShopContextProvider;
