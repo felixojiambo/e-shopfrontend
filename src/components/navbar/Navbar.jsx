f386e17794e6e7a7b8dac92ede8d80261ba62e3d
Felix Ojiambo <felixojiamboe@gmail.com>
2024-04-21 11:59:14 -0700

    making nav bar responsive for different components

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,19 +1,26 @@
-import React, { useContext, useState } from "react";
+import React, { useContext, useState ,useRef} from "react";
 import "./Navbar.css";
 import logo from "../assets/logo.png";
 import cart_icon from "../assets/cart_icon.png";
 import { Link } from "react-router-dom";
 import { ShopContext } from "../../context/ShopContext";
+import  nav_dropdown from "../assets/nav_dropdown.png"
 const Navbar = () => {
   const [menu, setMenu] = useState("shop");
   const {getTotalCartItems}=useContext(ShopContext);
+  const menuRef=useRef();
+  const dropdown_toggle=(e)=>{
+    menuRef.current.classList.toggle('nav-menu-visible');
+    e.target.classList.toggle('open');
+  }
   return (
     <div className="navbar">
       <div className="nav-logo">
         <img src={logo} alt="" />
         <p>E-SHOP</p>
       </div>
-      <uL className="nav-menu">
+      <img className="nav-dropdown" onClick={dropdown_toggle} src={nav_dropdown} alt=""/>
+      <ul ref={menuRef} className="nav-menu">
         <li
           onClick={() => {
             setMenu("shop");
@@ -47,7 +54,7 @@ const Navbar = () => {
             Kids
           </Link>{menu === "kids" ? <hr /> : <></>}
         </li>
-      </uL>
+      </ul>
       <div className="nav-login-cart">
         <Link to="/login">
           <button>Login</button>
