616f7d435551c4fdafa6b980dfa5dd526927a3e9
Felix Ojiambo <felixojiamboe@gmail.com>
2024-04-28 08:30:15 -0700

    fetch sign up api from backed

--- src/pages/LoginSignUp.jsx
+++ src/pages/LoginSignUp.jsx
@@ -16,6 +16,19 @@ const LoginSignUp = () => {
   };
   const signup = async () => {
     console.log("Signup running", formData);
+    let responseData;
+    await fetch('http//localhost:4000/signup',{
+      method:'POST',
+      headers:{
+        Accept:'application/form-data',
+        'Content-Type':'application/json',
+      },
+      body:JSON.stringify(formData)
+    }).then((response)=>response.json()).then((data)=>responseData=data)
+    if(responseData.success){
+      localStorage.setItem('auth-token',responseData.token);
+      window.location.replace("/");
+    }
   };
   return (
     <div className="loginsignup">
