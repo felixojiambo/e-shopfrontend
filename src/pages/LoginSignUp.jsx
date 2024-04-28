3790b9919fbc4d968a394fb31ce3d3412a207518
Felix Ojiambo <felixojiamboe@gmail.com>
2024-04-28 08:36:47 -0700

    fetch login api from backed

--- src/pages/LoginSignUp.jsx
+++ src/pages/LoginSignUp.jsx
@@ -13,6 +13,22 @@ const LoginSignUp = () => {
   };
   const login = async () => {
     console.log("login function executed", formData);
+    let responseData;
+    await fetch('http//localhost:4000/login',{
+      method:'POST',
+      headers:{
+        Accept:'application/form-data',
+        'Content-Type':'application/json',
+      },
+      body:JSON.stringify(formData)
+    }).then((response)=>response.json()).then((data)=>responseData=data)
+    if(responseData.success){
+      localStorage.setItem('auth-token',responseData.token);
+      window.location.replace("/");
+    }
+    else{
+      alert(responseData.errors)
+    }
   };
   const signup = async () => {
     console.log("Signup running", formData);
@@ -28,6 +44,9 @@ const LoginSignUp = () => {
     if(responseData.success){
       localStorage.setItem('auth-token',responseData.token);
       window.location.replace("/");
+    }
+    else{
+      alert(responseData.errors)
     }
   };
   return (
