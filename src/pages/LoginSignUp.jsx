2966e18d3a45ce19d435df20895ed6e0b0521387
Felix Ojiambo <felixojiamboe@gmail.com>
2024-04-28 08:07:07 -0700

    hide state based on login or sign up

--- src/pages/LoginSignUp.jsx
+++ src/pages/LoginSignUp.jsx
@@ -1,28 +1,39 @@
-import React from 'react'
-import './css/LoginSignUp.css'
+import React from "react";
+import "./css/LoginSignUp.css";
+import { useState } from "react";
 const LoginSignUp = () => {
-
-  const [state,setState]=useState("Login")
+  const [state, setState] = useState("Login");
   return (
     <div className="loginsignup">
-
-      <div className="loginsignup-container"> <h1>{state} </h1>
-      
-      <div className="loginsignup-fields">
-        <input type="tetx" placeholder="Your name" />
-        <input type="email" placeholder="Email Address" />
-        <input type="password" placeholder="Password" />
+      <div className="loginsignup-container">
+        {" "}
+        <h1>{state} </h1>
+        <div className="loginsignup-fields">
+          {state === "Sign Up" ? (
+            <input type="tetx" placeholder="Your name" />
+          ) : (
+            <></>
+          )}
+          <input type="email" placeholder="Email Address" />
+          <input type="password" placeholder="Password" />
+        </div>
+        <button>Continue</button>
+        {state === "Sign Up" ? 
+          <p className="loginsignup-login">
+            Already have an account? <span>Login here</span>
+          </p>
+         : 
+          <p className="loginsignup-login">
+            Create an account? <span>Click here</span>
+          </p>
+        }
+        <div className="loginsignup-agree">
+          <input type="checkbox" name="" id="" />
+          <p>By continue, I agree to follow the terms and privacy policy</p>
+        </div>
       </div>
-      <button>Continue</button>
-
-      <p className="loginsignup-login">Already have an account? <span>Login here</span></p>
-      <div className="loginsignup-agree">
-        <input type="checkbox" name="" id="" />
-        <p>By continue, I agree to follow the terms and privacy policy</p>
-      </div>
-       </div>
     </div>
-  )
-}
+  );
+};
 
-export default LoginSignUp
+export default LoginSignUp;
